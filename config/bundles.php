fcc57367ea171ecbb61e0c17db41dc60500b11ed
David Romaní <david@flux.cat>
2020-05-12 13:08:32 +0200

    add basic entities

--- config/bundles.php
+++ config/bundles.php
@@ -24,4 +24,5 @@ return [
     Sonata\EasyExtendsBundle\SonataEasyExtendsBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Sonata\UserBundle\SonataUserBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
---
+++ src/Repository/ServiceRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Query;
+
+/**
+ * Class ServiceRepository.
+ *
+ * @category Repository
+ */
+class ServiceRepository extends EntityRepository
+{
+    /**
+     * @return QueryBuilder
+     */
+    public function findAllEnabledSortedByPositionQB()
+    {
+        return $this->createQueryBuilder('s')
+            ->where('s.enabled = :enabled')
+            ->setParameter('enabled', true)
+            ->orderBy('s.position', 'ASC');
+    }
+
+    /**
+     * @return Query
+     */
+    public function findAllEnabledSortedByPositionQ()
+    {
+        return $this->findAllEnabledSortedByPositionQB()->getQuery();
+    }
+
+    /**
+     * @return array
+     */
+    public function findAllEnabledSortedByPosition()
+    {
+        return $this->findAllEnabledSortedByPositionQ()->getResult();
+    }
+}
