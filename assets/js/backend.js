dcafa0f97a8ba674893e40b38fb372a52dc7f856
David Romaní <david@flux.cat>
2020-05-18 20:39:31 +0200

    fix backend JS assets problems

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,21 +1,15 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/backend.less';
+
 const routes = require('../../public/js/fos_js_routes.json');
 import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // require jQuery normally
-const $ = require('jquery');
+//const $ = require('jquery');
 
 // create global $ and jQuery variables
-global.$ = global.jQuery = $;
+//global.$ = global.jQuery = $;
 global.Routing = global.Routing = Routing;
 
-// console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+console.log('Hello Backend Webpack Encore! Edit me in assets/js/app.js');
