415b6b4ac27a5527cde433a327a2f27cde32239c
David Romaní <david@flux.cat>
2020-05-20 18:07:20 +0200

    fix PDFJS lib usage

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -3,6 +3,7 @@ import '../css/backend.less';
 
 const routes = require('../../public/js/fos_js_routes.json');
 import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+import PDFJS from 'pdfjs-dist';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // require jQuery normally
@@ -11,5 +12,6 @@ import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/pu
 // create global $ and jQuery variables
 //global.$ = global.jQuery = $;
 global.Routing = global.Routing = Routing;
+global.PDFJS = global.PDFJS = PDFJS;
 
 console.log('Hello Backend Webpack Encore! Edit me in assets/js/app.js');
