b2426e4b8837d29c1b0d2003563c38a56df3e774
David Romaní <david@flux.cat>
2020-05-21 16:03:44 +0200

    hide test email route in prod env

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,17 +1,14 @@
-// any CSS you import will output into a single css file (app.css in this case)
 import '../css/backend.less';
 
-const routes = require('../../public/js/fos_js_routes.json');
-import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+// const routes = require('../../public/js/fos_js_routes.json');
+// import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
 import PDFJS from 'pdfjs-dist';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // require jQuery normally
-//const $ = require('jquery');
+// const $ = require('jquery');
 
 // create global $ and jQuery variables
-//global.$ = global.jQuery = $;
-global.Routing = global.Routing = Routing;
+// global.$ = global.jQuery = $;
+// global.Routing = global.Routing = Routing;
 global.PDFJS = global.PDFJS = PDFJS;
-
-console.log('Hello Backend Webpack Encore! Edit me in assets/js/app.js');
