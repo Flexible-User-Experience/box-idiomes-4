882191fabccc467d6b7a2a107412e69212d9d213
David Romaní <david@flux.cat>
2022-02-03 17:40:30 +0100

    fix frontend jQuery misssing dependecy

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,5 +1,19 @@
 import '../css/frontend.scss';
-import '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap';
-const $ = require('jquery');
+import jQuery from 'jquery';
 
-global.$ = global.jQuery = $;
+jQuery(document).ready(function() {
+    jQuery('.scroll-top-wrapper').on('click', scrollToTop);
+    function scrollToTop() {
+        const element = jQuery('body');
+        const offset = element.offset();
+        const offsetTop = offset.top;
+        jQuery('html, body').animate({scrollTop: offsetTop}, 1000, 'swing');
+    }
+});
+jQuery(document).on('scroll', function() {
+    if (jQuery(window).scrollTop() > 100) {
+        jQuery('.scroll-top-wrapper').addClass('show');
+    } else {
+        jQuery('.scroll-top-wrapper').removeClass('show');
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 }
 
 Encore
-    // directory where compiled assets will be stored
+    // disc copy
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .copyFiles([
@@ -13,28 +13,12 @@ Encore
         {from: './assets/svg', to: 'svg/[path][name].[ext]'},
         {from: './assets/fonts', to: 'fonts/[path][name].[ext]'},
     ])
-    /*
-     * ENTRY CONFIG
-     */
+    // entries
     .addEntry('frontend', './assets/js/frontend.js')
     .addEntry('backend', './assets/js/backend.js')
     .addEntry('fullcalendardefaultsettings', './assets/js/fullcalendar.default-settings.js')
     .addEntry('fullcalendarstudentsettings', './assets/js/fullcalendar.student-settings.js')
-
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    .splitEntryChunks()
-
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
-
-    /*
-     * FEATURE CONFIG
-     */
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
+    // config
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
     })
@@ -42,15 +26,16 @@ Encore
         config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
-
-    // enables LESS support
-    .enableSassLoader()
-
-    // enables Stimulus support
+    // features
+    // .autoProvidejQuery()
+    .cleanupOutputBeforeBuild()
+    .splitEntryChunks()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
     .enableStimulusBridge('./assets/controllers.json')
-
-    // uncomment if you're having problems with a jQuery plugin
-    .autoProvidejQuery()
+    .enableSingleRuntimeChunk()
+    .enableSassLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
