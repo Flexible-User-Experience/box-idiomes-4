e2d357b79a4ae94c97bd9ed29d04fee0dc089ee4
David Romaní <david@flux.cat>
2022-02-04 10:28:43 +0100

    improve Bootstrap 5 update - 4

--- assets/js/scroll_to_flash.js
+++ assets/js/scroll_to_flash.js
@@ -1,18 +1,16 @@
 import jQuery from 'jquery';
 
 jQuery(document).ready(function() {
-    jQuery(document).ready(function() {
-        const errorNode = jQuery(".glyphicon-exclamation-sign");
-        const envelopeNode = jQuery(".fa-paper-plane-o");
-        const exclamationTriangleNode = jQuery(".fa-exclamation-triangle");
-        if (errorNode.length > 0) {
-            jQuery('html,body').animate({scrollTop: errorNode.offset().top - 100}, "slow");
-        }
-        if (envelopeNode.length > 0) {
-            jQuery('html,body').animate({scrollTop: envelopeNode.offset().top - 100}, "slow");
-        }
-        if (exclamationTriangleNode.length > 0) {
-            jQuery('html,body').animate({scrollTop: exclamationTriangleNode.offset().top - 100}, "slow");
-        }
-    });
+    const errorNode = jQuery('.glyphicon-exclamation-sign');
+    const envelopeNode = jQuery('.fa-paper-plane');
+    const exclamationTriangleNode = jQuery('.fa-exclamation-triangle');
+    moveToNode(errorNode);
+    moveToNode(envelopeNode);
+    moveToNode(exclamationTriangleNode);
 });
+
+function moveToNode(node) {
+    if (node.length > 0) {
+        jQuery('html,body').animate({scrollTop: node.offset().top - 100}, 'slow');
+    }
+}
