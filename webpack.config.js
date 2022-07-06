a20354512dc3ec02a54cf660cff15bbc1d6c8f75
David Romaní <david@flux.cat>
2022-07-06 13:29:39 +0200

    improve Symfony UX Charts usage 3

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ Encore
     // disc copy
     .setOutputPath('public/build/')
     .setPublicPath('/build')
-<<<<<<< ours
     .copyFiles([
         {from: './assets/img', to: 'img/[path][name].[ext]'},
         {from: './assets/svg', to: 'svg/[path][name].[ext]'},
@@ -30,47 +29,14 @@ Encore
     })
     // features
     // .autoProvidejQuery()
-=======
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
-
-    /*
-     * ENTRY CONFIG
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
-    .addEntry('app', './assets/app.js')
-
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
-
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    .splitEntryChunks()
-
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
-
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
->>>>>>> theirs
     .cleanupOutputBeforeBuild()
     .splitEntryChunks()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-<<<<<<< ours
     .enableStimulusBridge('./assets/controllers.json')
     .enableSingleRuntimeChunk()
     .enableSassLoader()
-=======
-
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
     })
@@ -96,7 +62,6 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
->>>>>>> theirs
 ;
 
 module.exports = Encore.getWebpackConfig();
