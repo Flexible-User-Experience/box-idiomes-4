72001e7a1a5947892ae7c1ed54f3f54d5aed50f0
David Romaní <david@flux.cat>
2022-08-18 09:08:30 +0200

    vendors update

--- config/bundles.php
+++ config/bundles.php
@@ -11,6 +11,7 @@ return [
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['test' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['test' => true],
     Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
@@ -36,5 +37,4 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
