47d9239711d38c07068031948537da3f00c585cf
David Romaní <david@flux.cat>
2023-02-21 17:24:17 +0100

    fix action column width in TrainingCenterAdmin list view

--- src/Admin/TrainingCenterAdmin.php
+++ src/Admin/TrainingCenterAdmin.php
@@ -155,6 +155,7 @@ final class TrainingCenterAdmin extends AbstractBaseAdmin
                 null,
                 [
                     'label' => 'backend.admin.actions',
+                    'header_style' => 'width:63px',
                     'header_class' => 'text-right',
                     'row_align' => 'right',
                     'actions' => [
