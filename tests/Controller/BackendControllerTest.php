5f2a1e67dcdabf26ff43b0ee73fd31aabe35f823
David Romaní <david@flux.cat>
2023-02-28 17:39:19 +0100

    Symfony v5.4.21 update

--- tests/Controller/BackendControllerTest.php
+++ tests/Controller/BackendControllerTest.php
@@ -67,6 +67,8 @@ class BackendControllerTest extends WebTestCase
             array('/admin/students/student/1/edit'),
             array('/admin/students/student/1/show'),
             array('/admin/students/student/1/delete'),
+            array('/admin/students/student/mailing'),
+            array('/admin/students/student/mailing-write'),
             array('/admin/students/absence/list'),
             array('/admin/students/absence/create'),
             array('/admin/students/absence/1/edit'),
