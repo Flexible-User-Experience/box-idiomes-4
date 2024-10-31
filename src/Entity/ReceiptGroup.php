0d9dcecb0d32d5693729930fe2c5fbbe80a83efe
David Romaní <david@flux.cat>
2024-10-31 10:26:14 +0100

    add ReceiptGroup entity & repository

---
+++ src/Entity/ReceiptGroup.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\MonthTrait;
+use App\Entity\Traits\TrainingCenterTrait;
+use App\Entity\Traits\YearTrait;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ReceiptGroupRepository")
+ * @ORM\Table(name="receipt_group")
+ */
+class ReceiptGroup extends AbstractBase
+{
+    use MonthTrait;
+    use TrainingCenterTrait;
+    use YearTrait;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Receipt", mappedBy="receiptGroup")
+     * @Assert\Valid
+     */
+    private ?Collection $receipts;
+
+    public function __construct()
+    {
+        $this->receipts = new ArrayCollection();
+    }
+
+    public function getReceipts(): ?Collection
+    {
+        return $this->receipts;
+    }
+
+    public function setReceipts(?Collection $receipts): self
+    {
+        $this->receipts = $receipts;
+
+        return $this;
+    }
+
+    public function addReceipt(Receipt $receipt): self
+    {
+        if (!$this->receipts->contains($receipt)) {
+            $receipt->setReceiptGroup($this);
+            $this->receipts->add($receipt);
+        }
+
+        return $this;
+    }
+
+    public function removeReceipt(Receipt $receipt): self
+    {
+        if ($this->receipts->contains($receipt)) {
+            $this->receipts->removeElement($receipt);
+        }
+
+        return $this;
+    }
+
+    public function calculateTotal(): float
+    {
+        $result = 0.0;
+        /** @var Receipt $receipt */
+        foreach ($this->receipts as $receipt) {
+            $result += $receipt->getBaseAmount();
+        }
+
+        return $result;
+    }
+
+    public function __toString(): string
+    {
+        return $this->id ? $this->getReceiptNumber().' · '.$this->getTrainingCenter().' · '.$this->getBaseAmountString() : AbstractBase::DEFAULT_NULL_STRING;
+    }
+}
