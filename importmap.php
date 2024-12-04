f9658e02ab1bdb15c560f0cf8196a63c2211ebb8
David Romaní <david@flux.cat>
2024-12-04 18:41:29 +0100

    add boostrap CSS

--- importmap.php
+++ importmap.php
@@ -40,4 +40,14 @@ return [
     '@kurkle/color' => [
         'version' => '0.3.4',
     ],
+    'bootstrap' => [
+        'version' => '5.3.3',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
 ];
