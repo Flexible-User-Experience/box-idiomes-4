a9ab11d1e8914613748062410bb315b49c3f010f
David Romaní <david@flux.cat>
2025-01-12 11:00:34 +0100

    composer dependencies update

--- assets/app.js
+++ assets/app.js
@@ -6,4 +6,4 @@
  */
 import './stimulus_bootstrap.js';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper v4! 🎉');
+console.log('This log comes from assets/app.js - welcome to AssetMapper v5! 🎉');
