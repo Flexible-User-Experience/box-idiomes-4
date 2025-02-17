6084b887686957aa385f0b9b9bf1a503f594081b
David Romaní <david@flux.cat>
2025-02-17 08:17:56 +0100

    fix Routing problem in events-student-assistance Stimulus controller

--- importmap.php
+++ importmap.php
@@ -16,57 +16,69 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
-    ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
-    'chart.js' => [
-        'version' => '4.4.7',
-    ],
-    'pdfjs-dist/build/pdf.min.mjs' => [
-        'version' => '4.10.38',
-    ],
-    'pdfjs-dist/build/pdf.worker.min.mjs' => [
-        'version' => '4.10.38',
-    ],
-    '@hotwired/turbo' => [
-        'version' => '8.0.12',
-    ],
-    'stimulus-use' => [
-        'version' => '0.52.3',
-    ],
-    '@kurkle/color' => [
-        'version' => '0.3.4',
-    ],
-    'bootstrap' => [
-        'version' => '5.3.3',
-    ],
-    '@popperjs/core' => [
-        'version' => '2.11.8',
-    ],
-    'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.3',
-        'type' => 'css',
-    ],
-    'axios' => [
-        'version' => '1.7.9',
-    ],
-    'fullcalendar' => [
-        'version' => '5.11.5',
-    ],
     '@fullcalendar/common' => [
         'version' => '5.11.5',
     ],
     '@fullcalendar/core' => [
         'version' => '5.11.5',
     ],
+    '@fullcalendar/core/locales/ca' => [
+        'version' => '5.11.5',
+    ],
     '@fullcalendar/daygrid' => [
         'version' => '5.11.5',
     ],
-    'tslib' => [
-        'version' => '2.8.1',
+    '@fullcalendar/google-calendar' => [
+        'version' => '5.11.5',
+    ],
+    '@fullcalendar/interaction' => [
+        'version' => '5.11.5',
+    ],
+    '@fullcalendar/list' => [
+        'version' => '5.11.5',
+    ],
+    '@fullcalendar/timegrid' => [
+        'version' => '5.11.5',
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '8.0.12',
+    ],
+    '@kurkle/color' => [
+        'version' => '0.3.4',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    '@toyokumo/fos-router' => [
+        'version' => '1.0.5',
+    ],
+    'axios' => [
+        'version' => '1.7.9',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.3',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
+    'chart.js' => [
+        'version' => '4.4.7',
+    ],
+    'fullcalendar' => [
+        'version' => '5.11.5',
+    ],
+    'pdfjs-dist/build/pdf.min.mjs' => [
+        'version' => '4.10.38',
+    ],
+    'pdfjs-dist/build/pdf.worker.min.mjs' => [
+        'version' => '4.10.38',
     ],
     'preact' => [
         'version' => '10.12.1',
@@ -77,22 +89,10 @@ return [
     'preact/hooks' => [
         'version' => '10.12.1',
     ],
-    '@fullcalendar/interaction' => [
-        'version' => '5.11.5',
+    'stimulus-use' => [
+        'version' => '0.52.3',
     ],
-    '@fullcalendar/timegrid' => [
-        'version' => '5.11.5',
-    ],
-    '@fullcalendar/list' => [
-        'version' => '5.11.5',
-    ],
-    '@fullcalendar/core/locales/ca' => [
-        'version' => '5.11.5',
-    ],
-    '@fullcalendar/google-calendar' => [
-        'version' => '5.11.5',
-    ],
-    '@toyokumo/fos-router' => [
-        'version' => '1.0.5',
+    'tslib' => [
+        'version' => '2.8.1',
     ],
 ];
