7b761b4b83ff3d7b1f9c830781fa3e998e81d8cb
David Romaní <david@flux.cat>
2025-03-28 18:01:04 +0100

    some void code

--- src/Repository/BankRepository.php
+++ src/Repository/BankRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Doctrine\Enum\SortOrderTypeEnum;
 use App\Entity\Bank;
 use App\Entity\Student;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -14,6 +15,11 @@ final class BankRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Bank::class);
+    }
+
+    public function getAllSortedByNameQB(): QueryBuilder
+    {
+        return $this->createQueryBuilder('b')->orderBy('b.name', SortOrderTypeEnum::ASC);
     }
 
     public function getStudentRelatedItemsQB(?Student $student = null): QueryBuilder
